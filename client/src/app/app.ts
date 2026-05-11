f55a0bea71cc5ced9455ca4c7206d1883af0208a
Osmar Aldair R.S <osmar.salazar@foxconn.com>
2026-05-11 16:47:00 -0700

    End of section 8

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Header } from './layout/header/header';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  products = signal<Product[]>([]);
+
+  protected readonly title = signal('Skinet');
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response) => this.products.set(response.data),
+      error: (error) => console.log(error),
+      complete: () => console.log('Complete'),
+    });
+  }
 }
