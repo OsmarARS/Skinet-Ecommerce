ddeb5ae86e9c8f45116370f446d2e73e1d1c9801
Osmar Aldair R.S <osmar.salazar@foxconn.com>
2026-05-18 11:51:23 -0700

    End of section 10

--- client/src/app/features/shop/product-item/product-item.ts
+++ client/src/app/features/shop/product-item/product-item.ts
@@ -8,6 +8,7 @@ import {
 import { CurrencyPipe } from '@angular/common';
 import { MatAnchor } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
@@ -18,6 +19,7 @@ import { MatIcon } from '@angular/material/icon';
     MatCardActions,
     MatAnchor,
     MatIcon,
+    RouterLink,
   ],
   templateUrl: './product-item.html',
   styleUrl: './product-item.css',
